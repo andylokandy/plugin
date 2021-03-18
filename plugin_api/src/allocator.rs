a07ca5b518998e63cb51cbfe8907e45b8250dd54
Andy Lok <andylokandy@hotmail.com>
2021-03-19 02:05:28 +0800

    Fix typo

--- plugin_api/src/allocator.rs
+++ plugin_api/src/allocator.rs
@@ -22,26 +22,26 @@ pub mod host_alloctor {
     use std::alloc::{GlobalAlloc, Layout};
     use std::sync::atomic::{AtomicUsize, Ordering};
 
-    #[global_allocator]
-    static A: HostAllocator = HostAllocator;
-
     static ALLOC_FN: AtomicUsize = AtomicUsize::new(0);
     static DEALLOC_FN: AtomicUsize = AtomicUsize::new(0);
+
+    #[doc(hide)]
+    pub unsafe fn set_allocator(allocator: HostAllocatorPtr) {
+        ALLOC_FN.store(allocator.alloc_fn as usize, Ordering::SeqCst);
+        DEALLOC_FN.store(allocator.dealloc_fn as usize, Ordering::SeqCst);
+    }
+
+    #[global_allocator]
+    static HOST_ALLOCATOR: HostAllocator = HostAllocator;
+
     struct HostAllocator;
 
     unsafe impl GlobalAlloc for HostAllocator {
         unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
             (std::mem::transmute::<_, AllocFn>(ALLOC_FN.load(Ordering::Relaxed)))(layout)
         }
-
         unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
             (std::mem::transmute::<_, DeallocFn>(DEALLOC_FN.load(Ordering::Relaxed)))(ptr, layout)
         }
-    }
-
-    #[doc(hide)]
-    pub unsafe fn set_allocator(allocator: HostAllocatorPtr) {
-        ALLOC_FN.store(allocator.alloc_fn as usize, Ordering::SeqCst);
-        DEALLOC_FN.store(allocator.dealloc_fn as usize, Ordering::SeqCst);
     }
 }
